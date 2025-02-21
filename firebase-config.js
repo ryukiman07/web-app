8435ec37d4fd28918d5c50446e7ea0c16df1d206
denryu07 <ryu_denya@fez-inc.jp>
2025-02-21 22:35:27 +0900

    second commit

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyC3uRy3VW5_9WKVdceKt4BI2BZRYz1CWoc",
-  authDomain: "web-app-dad25.firebaseapp.com",
-  projectId: "web-app-dad25",
-  storageBucket: "web-app-dad25.firebasestorage.app",
-  messagingSenderId: "1091131765465",
-  appId: "1:1091131765465:web:cc8df36374229dec1e04c8",
-  measurementId: "G-T7M2LTKP2Q"
+    apiKey: "AIzaSyC3uRy3VW5_9WKVdceKt4BI2BZRYz1CWoc",
+    authDomain: "web-app-dad25.firebaseapp.com",
+    projectId: "web-app-dad25",
+    storageBucket: "web-app-dad25.firebasestorage.app",
+    messagingSenderId: "1091131765465",
+    appId: "1:1091131765465:web:cc8df36374229dec1e04c8",
+    measurementId: "G-T7M2LTKP2Q"
 };
 
 // Firebase 初期化
